b09cff07e48b0dac99193eb139944fa737e25dc7
nm86nm <nm86nm@gmail.com>
2025-02-11 19:17:50 +0100

    Project improvement

---
+++ app/src/main/java/com/mnp/jetpackcompose/HelloWorld.kt
@@ -0,0 +1,29 @@
+package com.mnp.jetpackcompose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HelloWorld(innerPadding: PaddingValues){
+    Column (
+        //modifier = Modifier.padding(innerPadding)
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        repeat(10){counter ->
+
+            Text(
+                text = "$counter. Hello World!",
+                fontSize = 30.sp
+            )
+        }
+    }
+}
--- app/src/main/java/com/mnp/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/mnp/jetpackcompose/MainActivity.kt
@@ -4,17 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import com.mnp.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,30 +20,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
- fun HelloWorld(innerPadding: PaddingValues){
-    Column (
-        //modifier = Modifier.padding(innerPadding)
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ){
-        repeat(10){counter ->
-            Text(
-                text = "$counter. Hello World!",
-                fontSize = 30.sp
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackComposeTheme {
-        HelloWorld(PaddingValues())
     }
 }
